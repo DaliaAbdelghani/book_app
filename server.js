26572ace626cc3f00ee7d585bbe10a2db9decb45
DaliaAbdelghani <engdna87@gmail.com>
2021-04-04 19:54:11 +0300

    book app

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const superagent = require('superagent');
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.static('./public'));
@@ -21,11 +22,24 @@ app.get ('/index',(req, res)=>{
 
 // the search route
 app.get ('/searches/new',(req, res)=>{
-  res.sendFile('new.ejs');
+  res.sendFile('new.ejs', { root: 'views' });
 });
 
 // book constructor
 
 function Book (data){
-  
+  this.image=image;
+  this.title=title;
+  this.author=author;
+  this.description=description;
 }
+
+// finding the volume ID in the id URL parameter e.g->https://books.google.com/ebooks?id=<%id%>=holmes&as_brr=4&source=webstore_bookcard
+/* perform a volumes search by sending an HTTP GET request to the following URI
+https://www.googleapis.com/books/v1/volumes?q=<%search%>+<%terms%>*/
+let book = new Book();
+superagent.post('/searches')
+  .send(book)
+  .end(function(err, response) {
+    //upload done!
+  });
