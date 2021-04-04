f947a430010b9a8d342edbfefadebd3acf241767
DaliaAbdelghani <engdna87@gmail.com>
2021-04-04 18:17:58 +0300

    testing index.ejs

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
-app.use(express.static('public'))
+app.use(express.static('./public'));
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
@@ -14,4 +14,18 @@ app.listen(PORT, () => {
 
 app.set('view engine', 'ejs');
 
+// testing the app. before entering data
+app.get ('/index',(req, res)=>{
+  res.send('HELLO');
+});
 
+// the search route
+app.get ('/searches/new',(req, res)=>{
+  res.sendFile('new.ejs');
+});
+
+// book constructor
+
+function Book (data){
+  
+}
